cfe6ae625beca28d702dc09d6a26790b6e0488f7
AnnaYal5 <gannam08@gmail.com>
2026-04-26 10:54:29 +0300

    added part2

---
+++ RPG-hero-inventory/RPG-hero-inventory/Inventory.cs
@@ -0,0 +1,49 @@
+namespace RPG_hero_inventory;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Inventory<T> : IEnumerable<T> where T : Item
+{
+    private List<T> _items = new List<T>();
+    private decimal _MaxWeight;
+
+    public Inventory(decimal maxWeight)
+    {
+        _MaxWeight = maxWeight;
+    }
+    private decimal CurrentWeight => _items.Sum(i => i.Weight);
+
+    public void Add(T item)
+    {
+        if (CurrentWeight + item.Weight <= _MaxWeight)
+        { 
+            _items.Add(item);
+        }
+        else
+        {
+            Console.WriteLine($"вагу перевищено");
+        }
+    }
+
+    public void Remove(T item)
+    {
+        _items.Remove(item);
+    }
+
+    public T GetBYName(string name)
+    {
+        return _items.FirstOrDefault(i => i.Name == name);
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        return _items.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+    
+}
